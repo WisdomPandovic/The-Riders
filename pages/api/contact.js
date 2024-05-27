fa3f57ad3199a359d85981900a7d3db4afc2e5ce
WisdomPandovic <wisdompandovic@gmail.com>
2024-05-27 11:27:20 +0100

    updated code

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -1,5 +1,10 @@
+import express from 'express';
 import mongoose from 'mongoose';
 import Contact  from '../../src/app/models/contact'; 
+import sendConfirmationEmail from '../../src/utils/emailService';
+
+const app = express();
+app.use(express.json());
 
 const connectDB = async () => {
   try {
@@ -22,6 +27,17 @@ export default async function handler(req, res) {
       const newContact = new Contact({ name, email, message });
 
       const savedContact = await newContact.save();
+
+            // Send a confirmation email
+            const emailSubject = 'Contact Form Submission';
+            const emailText = `Hello ${name},\n\nThank you for reaching out to us. We have received your message and will get back to you shortly.\n\nBest regards,\nThe Riders Team`;
+      
+            try {
+              await sendConfirmationEmail(email, emailSubject, emailText);
+              console.log('Confirmation email sent successfully');
+            } catch (error) {
+              console.error('Error sending confirmation email:', error);
+            }
 
       res.status(201).json({ message: 'Contact created successfully!', contact: savedContact });
     } catch (error) {
