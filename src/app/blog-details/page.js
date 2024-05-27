640cb397025d69a48fbd1093ca0aca21c305178f
WisdomPandovic <wisdompandovic@gmail.com>
2024-05-27 01:04:00 +0100

    updated code

--- src/app/blog-details/page.js
+++
@@ -1,9 +0,0 @@
-import BlogDetails from "../components/blogComponent/BlogDetails";
-function page (){
-    return(
-        <div>
-           <BlogDetails/>
-        </div>
-    )
-}
-export default page;
