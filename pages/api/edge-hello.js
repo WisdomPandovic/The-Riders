3ee53e13e17a278ee6aecd451b4a07e348907408
WisdomPandovic <wisdompandovic@gmail.com>
2024-05-29 12:16:21 +0100

    kode

--- pages/api/edge-hello.js
+++
@@ -1,7 +0,0 @@
-// api/edge-hello.js
-export default async function handler(req, event) {
-    return new Response('Hello, world!', {
-      headers: { 'content-type': 'text/plain' },
-    });
-  }
-  
