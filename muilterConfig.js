bf82e665655f68996efff5512a51be1e156c37a3
WisdomPandovic <wisdompandovic@gmail.com>
2024-05-31 23:33:53 +0100

    updated multer config for vercel

--- muilterConfig.js
+++ muilterConfig.js
@@ -1,9 +1,44 @@
+// import multer from 'multer';
+// import path from 'path';
+// import fs from 'fs';
+
+// // Define the upload directory
+// const uploadDirectory = path.join(__dirname, '..', '..', '..', 'public', 'uploads');
+
+// // Ensure the upload directory exists
+// if (!fs.existsSync(uploadDirectory)) {
+//   fs.mkdirSync(uploadDirectory, { recursive: true });
+// }
+
+// // Define storage for multer
+// const storage = multer.diskStorage({
+//   destination: (req, file, cb) => {
+//     cb(null, './public/uploads'); // Destination directory
+//   },
+//   filename: (req, file, cb) => {
+//     const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
+//     cb(null, uniqueSuffix + '-' + file.originalname);
+//   }
+// });
+
+// // Initialize multer with storage settings
+// const upload = multer({ 
+//   storage, 
+//   limits: { fileSize: 5 * 1024 * 1024 } // 5MB limit per file
+// });
+
+// // upload.fields([
+// //       { name: 'image', maxCount: 1 },
+// //     ]);
+
+// export default upload;
+
 import multer from 'multer';
 import path from 'path';
 import fs from 'fs';
 
-// Define the upload directory
-const uploadDirectory = path.join(__dirname, '..', '..', '..', 'public', 'uploads');
+// Define the upload directory using /tmp, which is writable in Vercel
+const uploadDirectory = path.join('/tmp', 'uploads');
 
 // Ensure the upload directory exists
 if (!fs.existsSync(uploadDirectory)) {
@@ -13,7 +48,7 @@ if (!fs.existsSync(uploadDirectory)) {
 // Define storage for multer
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, './public/uploads'); // Destination directory
+    cb(null, uploadDirectory); // Destination directory
   },
   filename: (req, file, cb) => {
     const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
@@ -27,8 +62,10 @@ const upload = multer({
   limits: { fileSize: 5 * 1024 * 1024 } // 5MB limit per file
 });
 
-// upload.fields([
-//       { name: 'image', maxCount: 1 },
-//     ]);
+// Example usage
+// app.post('/upload', upload.single('file'), (req, res) => {
+//   res.send('File uploaded successfully.');
+// });
 
 export default upload;
+
