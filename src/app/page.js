de08b5a2a956bd0e474da4d69c9b95605dcd8fc1
WisdomPandovic <wisdompandovic@gmail.com>
2024-06-23 00:15:39 +0100

    updated

--- src/app/page.js
+++ src/app/page.js
@@ -1,44 +1,35 @@
 "use client";
-// import Image from "next/image";
-// import { useRouter } from "next/navigation";
-
-// export default function Home() {
-//   const router = useRouter();
-
-//   router.replace("/home");
-//   return (
-//     <div className="d-flex items-center justify-content-center h-[100vh] mb-5 mt-5">
-//       <Image
-//         src={"/images/about-img.jpg"}
-//         width={100}
-//         height={100}
-//         alt="logo"
-//       />
-//     </div>
-//   );
-// }
-
 import { useRouter } from "next/navigation";
 import { useEffect } from "react";
+import Image from "next/image";
 
 export default function Home() {
   const router = useRouter();
 
-  // Assuming you're redirecting to "/home"
   useEffect(() => {
     // This ensures the code only runs on the client side
     router.replace("/home");
   }, [router]);
 
   return (
-    <div className="d-flex flex-column items-center justify-content-center h-screen mb-5 mt-5">
-      <h1 className="text-4xl font-bold text-center mb-5 pt-5">
-        Book Your Airport Ride with Ease
-      </h1>
-      <p className="text-lg text-center">
-        Welcome to The Riders Booking App, where booking your airport transportation is
-        simple and hassle-free.
-      </p>
+    <div className="d-flex flex-column items-center justify-content-center h-screen mt-3 mb-5">
+      <div className="text-center mb-5">
+        <h1 className="text-4xl font-bold">Book Your Airport Ride with Ease</h1>
+        <p className="text-lg">
+          Welcome to The Riders Booking App, where booking your airport transportation is
+          simple and hassle-free.
+        </p>
+      </div>
+
+      <div className="d-flex justify-content-center">
+        <Image
+          src={"/images/the-riders.jpg"}
+          width={100}
+          height={100}
+          alt="logo"
+          className="rounded-circle"
+        />
+      </div>
     </div>
   );
 }
